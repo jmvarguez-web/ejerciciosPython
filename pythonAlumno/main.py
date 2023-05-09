452c0ba038ea441a453cd08fcd8ba6e16215969f
jmvarguez <jmvarguez@hotmail.com>
2023-05-09 10:52:15 -0600

    ejercicio Alumno python validacion y mostrar datos

--- pythonAlumno/main.py
+++ pythonAlumno/main.py
@@ -1,28 +1,41 @@
-#En este ejercicio vais a crear la clase Vehículo la cual tendrá los siguientes atributos:
-#Color
-#Ruedas
-#Puertas
-#Por otro lado crearéis la clase Coche la cual heredará de Vehículo y tendrá los siguientes atributos:
-#Velocidad
-#Cilindrada
-#Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.*/
+print("En este segundo ejercicio, tendréis','que crear un programa que tenga una clase llamada"+
+" Alumno \nque tenga como atributos su nombre y su nota.\n"+
+"Deberéis de definir los métodos para inicializar sus atributos, imprimirlos y mostrar un mensaje\n"+
+"con el resultado de la nota y si ha aprobado o no.")
+class Alumno:
+    nombre = "none"
+    nota = 0
+    def __int__(self):
+        self.nombre = "falta nombre"
+        self.nota = 0
+    def inicia (self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
+    def imprimirNota(self):
+        print("Datos del Alumno:\n")
+        print("Nombre",self.nombre)
+        print("Nota:",self.nota)
+    def validaNota(self):
+        if self.nota < 6:
+            print("Aprobado:\n")
+        else:
+            print("No Aprobado:\n")
 
-class Vehículo:
-    _color ="rojo"
-    _ruedas=4
-    _puertas=4
-class Coche(Vehículo):
-    Velocidad =120
-    Cilindrada=2
+#creamos objetos
+alumno1=Alumno()
+alumno2=Alumno()
+alumno3=Alumno()
 
+#iniciamosy pasamos valores
+alumno1.inicia("Jesus",10)
+alumno2.inicia("Manuel",5.9)
 
-ver=Coche()
-ver._color
-ver._ruedas
-ver._puertas
-ver.Velocidad
-ver.Cilindrada
-print(dir(ver))
-print("\nEl coche es de color ",ver._color,",tiene ",ver._ruedas," ruedas ,",ver._puertas," ruedas\n y su velocidad es de ",ver.Velocidad,"y Cilindrada de ",ver.Cilindrada)
+#imprimimos notas y resultado de validacion de nota
 
-
+alumno1.imprimirNota();
+alumno1.validaNota();
+alumno2.imprimirNota();
+alumno2.validaNota();
+#como no se pasaron o se inicializaron valoresel contructor inicializa valores por defaul
+alumno3.imprimirNota();
+alumno3.validaNota();
